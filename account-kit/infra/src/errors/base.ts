587f849346878eb64969cd12575121c64b398f8a
Ayodeji63 <olusanyasamson633@gmail.com>
2025-12-04 14:08:54 +0100

    packages_not_commited

--- account-kit/infra/src/errors/base.ts
+++
@@ -1,6 +0,0 @@
-import { BaseError as CoreBaseError } from "@aa-sdk/core";
-
-export abstract class BaseError extends CoreBaseError {
-  // This version could be different from the aa-core version so we overwrite this here.
-  override version = "0.0.1";
-}
