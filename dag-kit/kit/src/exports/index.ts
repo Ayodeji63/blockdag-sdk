98b9a66e478a89b5b015e5a3940b020d4b537043
Ayodeji63 <olusanyasamson633@gmail.com>
2025-12-23 13:55:23 +0100

    change_version2

--- dag-kit/kit/src/exports/index.ts
+++ dag-kit/kit/src/exports/index.ts
@@ -10,3 +10,7 @@ export {
 export { awakening, arbitrumSep } from "../clients/chains.js";
 
 export { DagAAClient } from "../clients/actions/main.js";
+
+export { PrivySigner } from "../signers/PrivySigner.js";
+
+export { PrivateKeySigner } from "../signers/PrivateKeySigner.js";
